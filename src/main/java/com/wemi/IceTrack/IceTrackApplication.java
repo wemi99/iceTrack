44fd787737063c6353d058f7f8d5ab093a60866a
Wemi Oshun-Williams <ooo29@cornell.edu>
2025-08-28 23:17:14 +0100

    project setup - initial commit

---
+++ src/main/java/com/wemi/IceTrack/IceTrackApplication.java
@@ -0,0 +1,13 @@
+package com.wemi.IceTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IceTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IceTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wemi/IceTrack/IceTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wemi.IceTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IceTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
